451660c5a843a0d1b702641dcf6e08b1b01779ce
Radek <michalski.rados@gmail.com>
2018-12-06 15:24:45 +0100

    added Pets and Contact info to owners on startup. Closes #33

--- pet-clinic-web/src/main/java/radek/spring/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/radek/spring/petclinic/bootstrap/DataLoader.java
@@ -3,11 +3,14 @@ package radek.spring.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import radek.spring.petclinic.model.Owner;
+import radek.spring.petclinic.model.Pet;
 import radek.spring.petclinic.model.PetType;
 import radek.spring.petclinic.model.Vet;
 import radek.spring.petclinic.services.OwnerService;
 import radek.spring.petclinic.services.PetTypeService;
 import radek.spring.petclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,14 +39,35 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Pet Type loaded...");
 
         Owner owner1 = new Owner();
-        owner1.setFirstName("ryszard");
-        owner1.setLastName("kalisz");
+        owner1.setFirstName("Ryszard");
+        owner1.setLastName("Kalisz");
+        owner1.setAddress("123 Street");
+        owner1.setCity("Gdynia");
+        owner1.setCity("123456789");
+
+        Pet richsDog = new Pet();
+        richsDog.setPetType(saveDogType);
+        richsDog.setOwner(owner1);
+        richsDog.setBirthDate(LocalDate.now());
+        richsDog.setName("Loco");
+        owner1.getPets().add(richsDog);
+
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Krysztof");
         owner2.setLastName("Jerzyna");
+        owner2.setAddress("312 Street");
+        owner2.setCity("Gdanks");
+        owner2.setCity("123123456");
+
+        Pet krisCat = new Pet();
+        krisCat.setName("Klakier");
+        krisCat.setOwner(owner2);
+        krisCat.setBirthDate(LocalDate.now());
+        krisCat.setPetType(saveCatType);
+        owner2.getPets().add(krisCat);
 
         ownerService.save(owner2);
 
