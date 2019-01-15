f5126af4c74bebf74a30bc37e9892cc9e09ecbd1
mi7r <michalski.rados@gmail.com>
2019-01-15 11:18:09 +0100

    Upgrade to JUnit 5. Closes #4

--- pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -1,12 +1,14 @@
 package petclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import radek.spring.petclinic.PetClinicApplication;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = PetClinicApplication.class)
 public class PetClinicApplicationTests {
 
     @Test
