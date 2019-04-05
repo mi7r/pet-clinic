b34f12076824fefb767caad6068136e00ee3feb2
Radek <michalski.rados@gmail.com>
2019-04-05 13:33:45 +0200

    Pet Controller updated. Add Pet implemented. Closes #55

--- pet-clinic-data/src/main/java/radek/spring/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/radek/spring/petclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package radek.spring.petclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -10,10 +11,19 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity{
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.visits = visits;
+    }
 
     @Column(name = "name")
     private String name;
@@ -27,6 +37,7 @@ public class Pet extends BaseEntity{
     private Owner owner;
 
     @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
---
+++ pet-clinic-web/src/main/java/radek/spring/petclinic/formaters/PetTypeFormatter.java
@@ -0,0 +1,37 @@
+package radek.spring.petclinic.formaters;
+
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+import radek.spring.petclinic.model.PetType;
+import radek.spring.petclinic.services.PetTypeService;
+
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.Locale;
+
+@Component
+public class PetTypeFormatter implements Formatter<PetType> {
+
+    PetTypeService petTypeService;
+
+    public PetTypeFormatter(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
+    @Override
+    public PetType parse(String text, Locale locale) throws ParseException {
+        Collection<PetType> findPetTypes = petTypeService.findAll();
+
+        for (PetType petType : findPetTypes) {
+            if (petType.getName().equals(text)) {
+                return petType;
+            }
+        }
+        throw new ParseException("type not found: " + text, 0);
+    }
+
+    @Override
+    public String print(PetType petType, Locale locale) {
+        return petType.getName();
+    }
+}
