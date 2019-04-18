ee572e67ee612f48cd12f94f3590dfec8bfccf81
Radek <michalski.rados@gmail.com>
2019-04-18 11:50:36 +0200

    Visit Controller Test implemented.

--- pet-clinic-web/src/main/java/radek/spring/petclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/radek/spring/petclinic/controllers/VisitController.java
@@ -10,6 +10,8 @@ import radek.spring.petclinic.services.PetService;
 import radek.spring.petclinic.services.VisitService;
 
 import javax.validation.Valid;
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.util.Map;
 
 @Controller
@@ -24,8 +26,15 @@ public class VisitController {
     }
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder){
+    public void dataBinder(WebDataBinder dataBinder){
         dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport(){
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException{
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     @ModelAttribute("visit")
