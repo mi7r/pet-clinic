3209b67f976015afb7b9dbe6212469232a3bdbec
Radek <michalski.rados@gmail.com>
2019-04-26 12:23:28 +0200

    Implemented Vest JSON endpoint. Closes #61

--- pet-clinic-web/src/main/java/radek/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/radek/spring/petclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package radek.spring.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import radek.spring.petclinic.model.Vet;
 import radek.spring.petclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -19,5 +24,10 @@ public class VetController {
 
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
